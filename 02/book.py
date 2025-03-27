53d0041357f2caab8f8e73f0ad1e0d5e7ec0ba81
Jakub Gromala <jakubgromala@MacBook-Pro-Jakub-2.local>
2025-03-27 11:16:39 +0100

    zadanie_2

---
+++ 02/book.py
@@ -0,0 +1,19 @@
+class Book:
+
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def __str__(self):
+        return f""" "{self.title}" - {self.author} ({self.year})"""
+
+
+books = [
+    Book("Python dla początkujących", 'Jan Kowalski', 2020),
+    Book("Python dla początkujących v.2", 'Jan Kowalski', 2021),
+    Book("Python dla zaawansowanych", 'Jan Kowalski', 2025)
+]
+
+for book in books:
+    print(book.__str__())
